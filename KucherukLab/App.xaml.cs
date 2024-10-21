687b60b4210bccb7c2d8ae39ab61de4d86c005b9
Danylo Kucheruk <kucherukdanulo@gmail.com>
2024-10-21 19:52:44 +0300

    Add project files.

---
+++ KucherukLab/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KucherukLab
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ KucherukLab/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KucherukLab
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
